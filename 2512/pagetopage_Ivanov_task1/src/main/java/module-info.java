be27b44bbd73219c892c6bb25b03df8111aef02e
ReddVee <andrey7306iva9254@gmail.com>
2024-12-25 23:52:59 +0300

    Add files via upload

---
+++ 2512/pagetopage_Ivanov_task1/src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module ru.ivanov.pagetopage_ivanov_task1 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens ru.ivanov.pagetopage_ivanov_task1 to javafx.fxml;
+    exports ru.ivanov.pagetopage_ivanov_task1;
+    exports ru.ivanov.pagetopage_ivanov_task1.controller;
+    opens ru.ivanov.pagetopage_ivanov_task1.controller to javafx.fxml;
+}
